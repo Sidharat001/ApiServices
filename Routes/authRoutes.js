33be43397726a9d7da35204ce3c67fb4cb0fbe7c
Karthik <karthik@gmail.com>
2024-03-01 21:53:07 +0530

    create 404 routes

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const AuthController = require('../Controller/authController');
+const UserController = require('../Controller/UserController');
 const AuthMiddleware = require('../middleware/authMiddleware');
 
-router.post('/register', AuthController.register);
-router.post('/login', AuthController.login);
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
 router.get('/protected', AuthMiddleware.requireAuth, (req, res) => {
     res.json({ message: 'This is a protected route' });
+});
+
+// Handle 404 - Route not found
+router.all("*", (req, res) => {
+    res.status(404).json({ message: '404 Not Found', status: 404 });
 });
 
 module.exports = router;
